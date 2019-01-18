18cd6dfd553271befbe105fac01f60bef6869f73
JpatrickCM <jeanpatrick.94@gmail.com>
2019-01-17 20:07:27 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const argv = require('./config/yargs').argv;
+const colors = require('colors');
+
+const porHacer = require('./por-hacer/por-hacer');
+
+let comando = argv._[0];
+
+switch (comando) {
+    case 'crear':
+        let tarea = porHacer.crear(argv.descripcion);
+        console.log(tarea);
+        break;
+    case 'listar':
+        let listado = porHacer.getListado();
+        for (let tarea of listado) {
+            console.log('========== Por hacer =========='.green);
+            console.log(tarea.descripcion);
+            console.log('Estado:', tarea.descripcion);
+            console.log('============================'.green);
+        }
+
+        break;
+    case 'actualizar':
+        console.log('Actualiza una tarea por hacer');
+        break;
+    default:
+        console.log('Comando no es reconocido');
+}
---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+
+
+let listadoPorHacer = [];
+
+const guardarDB = () => {
+
+    let data = JSON.stringify(listadoPorHacer);
+
+    fs.writeFile('db/data.json', data, (err) => {
+        if (err) throw new Error('No se pudo grabar');
+    })
+}
+
+const cargarDB = () => {
+    try {
+        listadoPorHacer = require('../db/data.json');
+    } catch (error) {
+        listadoPorHacer = [];
+    }
+}
+
+const getListado = () => {
+    cargarDB();
+    return listadoPorHacer;
+}
+
+
+const crear = (descripcion) => {
+
+    cargarDB();
+
+    let porHacer = {
+        descripcion,
+        completado: false
+    }
+    listadoPorHacer.push(porHacer);
+
+    guardarDB(listadoPorHacer);
+
+    return porHacer;
+}
+
+module.exports = {
+    crear,
+    getListado
+}
